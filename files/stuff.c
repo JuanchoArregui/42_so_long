85648a10d9f2a80bad9282c22d241c1c1afcf8dd
jarregui <jarregui@student.42madrid.com>
2024-04-03 13:00:16 +0200

    wip

--- files/stuff.c
+++ files/stuff.c
@@ -6,7 +6,7 @@
 /*   By: jarregui <jarregui@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 19:53:40 by jarregui          #+#    #+#             */
-/*   Updated: 2024/04/03 12:30:07 by jarregui         ###   ########.fr       */
+/*   Updated: 2024/04/03 12:59:57 by jarregui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,14 @@ void	check_map_extension(char *file_name)
 		;
 	else
 		ft_error("extensión de archivo incorrecta. Debe ser .ber");
+}
+
+void	read_map(char *map, t_list *d)
+{
+	int		fd;
+	// char	*line;
+
+	fd = open(map, O_RDONLY);
+	if (fd < 0)
+		ft_free(d);
 }
